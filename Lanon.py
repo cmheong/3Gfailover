ff6be56846d6b9f8bf0a7570b7d0b95643034280
cmheong <57660801+cmheong@users.noreply.github.com>
2020-01-22 09:28:02 +0800

    Add files via upload

---
+++ Lanon.py
@@ -0,0 +1,11 @@
+from time import sleep
+import pifacedigitalio
+
+
+DELAY = 2.0  # seconds
+
+
+if __name__ == "__main__":
+    pifacedigital = pifacedigitalio.PiFaceDigital()
+    #  2020-01-20 Turn on SIKAMAT2 LAN switch
+    pifacedigital.leds[0].turn_off()
